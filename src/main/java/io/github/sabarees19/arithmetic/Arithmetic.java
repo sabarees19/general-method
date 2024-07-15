a7415cb9ee99aaebafe7feb11f2057a93b92634a
sabarees <sabareesv@piraiinfo.com>
2024-07-15 16:29:22 +0530

    basic two number aritmetic methods added

---
+++ src/main/java/io/github/sabarees19/arithmetic/Arithmetic.java
@@ -0,0 +1,21 @@
+package io.github.sabarees19.arithmetic;
+
+public class Arithmetic {
+
+    public int add(int int1, int int2) {
+        return int1 + int2;
+    }
+
+    public int minus(int int1, int int2) {
+        return int1 - int2;
+    }
+
+    public int multiply(int int1, int int2) {
+        return int1 * int2;
+    }
+
+    public int divide(int int1, int int2) {
+        return int1 / int2;
+    }
+
+}
