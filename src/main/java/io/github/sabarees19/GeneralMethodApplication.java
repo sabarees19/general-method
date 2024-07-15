4df981a04dd7dfa00e36719ee77fc6622e1e6bf9
sabarees <sabareesv@piraiinfo.com>
2024-07-15 16:07:06 +0530

    First commit

---
+++ src/main/java/io/github/sabarees19/GeneralMethodApplication.java
@@ -0,0 +1,13 @@
+package io.github.sabarees19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sabarees19/GeneralMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sabarees19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
